6ce9955b2b06879e8a4c4e22cfc6267a7d47b906
dharan1999 <dharanthakar96@gmail.com>
2022-07-12 15:37:29 -0400

    Added font Open Sans

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,14 +1,14 @@
 import './category-item.styles.scss';
 
 const CategoryItem = ({ category }) => {
-    const { imageUrl, Title } = category;
+    const { imageUrl, title } = category;
     return (
     <div className="category-container">
     <div className='background-image' style={{backgroundImage: `url(${imageUrl})`}}>
     </div>
     <div className="category-body-container">
       <h2>
-        {Title}
+        { title }
       </h2>
       <p>Show Now!</p>
     </div>
