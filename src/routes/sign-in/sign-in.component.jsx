33790029a11c1294d3c669fd4ca2bbca6901ee51
dharan1999 <dharanthakar96@gmail.com>
2022-07-18 17:58:55 -0400

    created the sign up and email authentication

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,20 +1,37 @@
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
 import {
+    auth,
     signInWithGooglePopup,
     createUserDocumentFromAuth,
+    signInWithGoogleRedirect
   } from '../../utils/firebase/firebase.utils';
   
+  import SignUpForm from '../../components/sign-up-form/sign-up-form-component';
   const SignIn = () => {
+    // useEffect(async () => {
+    //     const response = await getRedirectResult(auth);
+    //     if(response) {
+    //       const userDocRef = await createUserDocumentFromAuth(response.user);
+    //     }
+      
+    // },[]);
+
     const logGoogleUser = async () => {
       const {user} = await signInWithGooglePopup();
       //console.log(response);
       const userDocRef = await createUserDocumentFromAuth(user);
       //createUserProfileDocument(response);
     };
+
+    
   
     return (
       <div>
         <h1>Sign In Page</h1>
         <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+        {/* <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button> */}
+        <SignUpForm />
       </div>
     );
   };
